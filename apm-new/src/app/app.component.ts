fe65894644a00c047b86cd72f2341462fa70ee06
Sharon Kindles <shalaskin29@gmail.com>
2022-02-11 14:27:23 -0600

    Started

--- apm-new/src/app/app.component.ts
+++ apm-new/src/app/app.component.ts
@@ -8,6 +8,6 @@ import { Component } from "@angular/core";
   </div>
   `
 })
-export class AppComponenet {
+export class AppComponent {
   pageTitle: string = 'Acme Product Management';
 }
--- apm-new/src/app/app.module.ts
+++ apm-new/src/app/app.module.ts
@@ -8,7 +8,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule  // every browser application must import 
   ],
   providers: [],
   bootstrap: [AppComponent]
